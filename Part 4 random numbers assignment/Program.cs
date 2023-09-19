a0d5a8c8c6162522f921cbd5675e6f0db4887372
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-19 11:01:22 -0400

    Angry. Doubles won't work.

--- Part 4 random numbers assignment/Program.cs
+++ Part 4 random numbers assignment/Program.cs
@@ -11,18 +11,32 @@ namespace Part_4_random_numbers_assignment
         static void Main(string[] args)
         {
             Random generator = new Random();
-            int min, max, die1, die2;
-            string yn1, yn2;
+            int min, max, die1, die2, min2, max2;
+            string yn1, yn2, yn3;
             yn1 = "Y";
             yn2 = "Y";
+            
+
             
             Console.WriteLine("Please pick a min & max number.");
             min = Convert.ToInt32(Console.ReadLine());
             max = Convert.ToInt32(Console.ReadLine());
-            while (yn1 == "Y" || yn1 == "y")
+
+            if (min < max)
             {
-                Console.WriteLine("You got " + generator.Next(min, max) + ". Would you like a new number? (Y/N)?");
-                yn1 = Console.ReadLine();
+                while (yn1 == "Y" || yn1 == "y")
+                {
+                    Console.WriteLine("You got " + generator.Next(min, max) + ". Would you like a new number? (Y/N)?");
+                    yn1 = Console.ReadLine();
+                }
+            }
+            else if (max < min)
+            {
+                while (yn1 == "Y" || yn1 == "y")
+                {
+                    Console.WriteLine("You got " + generator.Next(max, min) + ". Would you like a new number? (Y/N)?");
+                    yn1 = Console.ReadLine();
+                }
             }
             while (yn2 == "Y" || yn2 == "y")
             {
@@ -31,6 +45,8 @@ namespace Part_4_random_numbers_assignment
                 Console.WriteLine("You rolled some dice. The first dice reads " + die1 + ", and the second dice reads " + die2 + ". Together they equal " + (die1 + die2) + ". Do you want to roll again? (Y/N)");
                 yn2 = Console.ReadLine();
             }
+            Console.WriteLine(generator.NextDouble());
+            Console.ReadLine();
         }
     }
 }
